8bb6e55a4b0f3b71f2dc5858a9316c6b94ee3c4e
Steven Osborne <steven.c.osborne@gmail.com>
2018-08-26 13:56:50 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) =>{
+    var now = new Date().toString();
+    var log = now + ' ' + req.method + ' ' + req.url;
+    console.log(log);
+    
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log')
+        }
+    });
+    next();
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+   return new Date().getFullYear(); 
+});
+
+hbs.registerHelper('screamIt', (text) => {
+   return text.toUpperCase();
+});
+
+app.get('/',(req,res) =>{
+    //res.send('<h1>Hello Steven</h1>');
+    
+    res.send({
+        name: 'Steven',
+        likes: [
+            'Food',
+            'Travel',
+            'Business'
+        ]
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/home', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to the site'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is online on port 3000');
+});
